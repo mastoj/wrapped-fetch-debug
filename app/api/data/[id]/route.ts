3b43e185999232bd5d0b014b45645f6827b8d0f5
Tomas Jansson <tomas.jansson@gmail.com>
2024-10-25 22:33:24 +0200

    Log in route

--- app/api/data/[id]/route.ts
+++ app/api/data/[id]/route.ts
@@ -2,5 +2,6 @@ import { NextRequest, NextResponse } from "next/server";
 
 export const GET = async (req: NextRequest) => {
   const path = req.nextUrl.pathname;
+  console.log("==> Getting some data: ", path);
   return NextResponse.json({ hello: "world", path });
 };
